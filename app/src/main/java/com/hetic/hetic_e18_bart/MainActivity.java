2dfecbc732820ca7145787e4bfc2389c2913cccf
Alex Berneau <haldir.alex@gmail.com>
2017-12-19 16:06:24 +0100

    :tada: project init

---
+++ app/src/main/java/com/hetic/hetic_e18_bart/MainActivity.java
@@ -0,0 +1,74 @@
+package com.hetic.hetic_e18_bart;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final int RC_SIGN_IN = 123;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            // already signed in
+            Log.d("AUTH", "USER LOGGED OUT");
+        } else {
+            // not signed in
+            startActivityForResult(AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setIsSmartLockEnabled(false)
+                            .setAvailableProviders(
+                                    Arrays.asList(
+                                            new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                            .build(),
+                    RC_SIGN_IN);
+        }
+        findViewById(R.id.log_out_button).setOnClickListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                // user logged in
+                Log.d("AUTH", auth.getCurrentUser().getEmail());
+            }
+            else {
+                // user not auth
+                Log.d("AUTH", "NOT AUTHENTICATED");
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.log_out_button) {
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Log.d("AUTH", "USER LOGGED OUT");
+                            finish();
+                        }
+                    });
+        }
+    }
+}
