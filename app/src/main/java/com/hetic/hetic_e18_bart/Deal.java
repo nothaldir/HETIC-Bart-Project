733e07c495e0b14e211fd4a66863fcd51fe3fc66
Tom Moraiz <tom.moraiz@hetic.net>
2017-12-20 14:32:26 +0100

    [MODIFIED] user can now add deals to the firebase database

--- app/src/main/java/com/hetic/hetic_e18_bart/Deal.java
+++ app/src/main/java/com/hetic/hetic_e18_bart/Deal.java
@@ -6,8 +6,35 @@ package com.hetic.hetic_e18_bart;
 
 public class Deal {
 
-    String titre;
-    int prix;
-    String description;
+    String dealId;
+    String dealTitre;
+    int dealPrix;
+    String dealDescription;
 
+    public Deal(){
+
+    }
+
+    public Deal(String dealId, String dealTitre, int dealPrix, String dealDescription) {
+        this.dealId = dealId;
+        this.dealTitre = dealTitre;
+        this.dealPrix = dealPrix;
+        this.dealDescription = dealDescription;
+    }
+
+    public String getDealId() {
+        return dealId;
+    }
+
+    public String getDealTitre() {
+        return dealTitre;
+    }
+
+    public int getDealPrix() {
+        return dealPrix;
+    }
+
+    public String getDealDescription() {
+        return dealDescription;
+    }
 }
