c781fa5a7685b648afe92a993c72cb5b0f39df6c
Alex Berneau <haldir.alex@gmail.com>
2017-12-20 02:42:47 +0100

    :construction: geofence wip

---
+++ app/src/main/java/com/hetic/hetic_e18_bart/GeofencingActivity.java
@@ -0,0 +1,19 @@
+package com.hetic.hetic_e18_bart;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by alex on 20/12/17.
+ */
+
+public class GeofencingActivity extends AppCompatActivity {
+
+    private static final String TAG = GeofencingActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geofencing);
+    }
+}
