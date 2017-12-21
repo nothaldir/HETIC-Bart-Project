81477dde5dcf8a1553db092b513a0fe9d2f229fc
Alex Berneau <haldir.alex@gmail.com>
2017-12-21 01:32:11 +0100

    :sparkles: init list fragment

--- app/src/main/java/com/hetic/hetic_e18_bart/ListFragment.java
+++ app/src/main/java/com/hetic/hetic_e18_bart/ListFragment.java
@@ -1,12 +1,20 @@
 package com.hetic.hetic_e18_bart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ListFragment extends Fragment {
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class ListFragment extends Fragment implements View.OnClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -14,9 +22,21 @@ public class ListFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_list, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        getView().findViewById(R.id.create_deal_button).setOnClickListener(this);
+    }
+
     public static Fragment newInstance() {
         ListFragment listFragment = new ListFragment();
         return listFragment;
     }
 
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.create_deal_button) {
+            Intent intent = new Intent(getActivity(), CreateDealActivity.class);
+            startActivity(intent);
+        }
+    }
 }
